f76b02eb0a5d46cddc54b3c866cf9b4aad99aa12
RicardoTaipe <rdavid28@hotmail.es>
2021-11-02 20:48:05 -0500

    add splash screen

---
+++ app/src/main/java/com/example/recipeappmvp/SplashScreenActivity.java
@@ -0,0 +1,23 @@
+package com.example.recipeappmvp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.recipeappmvp.home.MainActivity;
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash_screen);
+    new Handler(Looper.getMainLooper())
+        .postDelayed(() -> {
+          startActivity(new Intent(getApplicationContext(), MainActivity.class));
+          finish();
+        }, 2000);
+  }
+}
